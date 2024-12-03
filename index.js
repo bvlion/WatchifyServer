90319942ad65287f9108632dd9dbd309eeeebc8d
bvlion <bvltiggeari@ambitious-i.net>
2024-12-03 09:00:57 +0900

    Implementation of intervention and suspension notifications separately

--- index.js
+++ index.js
@@ -11,10 +11,34 @@ const db = admin.database()
 db.ref('/notifier').on('child_changed', async (changedSnapshot) => {
   const key = changedSnapshot.key
   const json = changedSnapshot.val()
-  logger.exec.info('key: ' + key)
+  logger.exec.info('start key: ' + key)
   logger.exec.info(json)
 
   delete json['timestamp']
+  json['type'] = 'start'
+  const message = {
+    token: (await db.ref('/fcmToken/' + key).once('value')).val(),
+    data: json,
+    android: { priority: 'high', ttl: 1 }
+  }
+
+  admin.messaging().send(message)
+    .then((response) => {
+      logger.exec.info('Successfully sent message:', response);
+    })
+    .catch((error) => {
+      logger.exec.warn('Error sending message:', error);
+    })
+})
+
+db.ref('/stop').on('child_changed', async (changedSnapshot) => {
+  const key = changedSnapshot.key
+  const json = changedSnapshot.val()
+  logger.exec.info('stop key: ' + key)
+  logger.exec.info(json)
+
+  delete json['timestamp']
+  json['type'] = 'stop'
   const message = {
     data: json,
     token: (await db.ref('/fcmToken/' + key).once('value')).val()
